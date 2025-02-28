11c2e113ad687a7a8f75b65894a03bf95cb1b086
asteryk <liangyuekang@foxmail.com>
2025-03-01 07:26:12 +0800

    first commit

---
+++ src/components/blind/index.tsx
@@ -0,0 +1,575 @@
+"use client";
+
+/*
+ * color-blind
+ *
+ * This source was copied from http://mudcu.be/sphere/js/Color.Blind.js
+ *
+ * It contains modifications for use in node.js.
+ *
+ * The original copyright is included below.
+ *
+ * Here is a license note copied/edited from (http://colorlab.wickline.org/colorblind/colorlab/engine.js)
+ *
+ *  20221013 UPDATE
+ *      HCIRN appears to no longer exist. This makes it impractical
+ *      for users to obtain permission from HCIRN in order to use
+ *      this file for commercial works. Instead:
+ *
+ *      This work is licensed under a
+ *      Creative Commons Attribution-ShareAlike 4.0 International License.
+ *      http://creativecommons.org/licenses/by-sa/4.0/
+ */
+/*
+
+    The Color Blindness Simulation function is
+    copyright (c) 2000-2001 by Matthew Wickline and the
+    Human-Computer Interaction Resource Network ( http://hcirn.com/ ).
+    
+    It is used with the permission of Matthew Wickline and HCIRN,
+    and is freely available for non-commercial use. For commercial use, please
+    contact the Human-Computer Interaction Resource Network ( http://hcirn.com/ ).
+
+	------------------------
+	blind.protan =
+		cpu = 0.735; // confusion point, u coord
+		cpv = 0.265; // confusion point, v coord
+		abu = 0.115807; // color axis begining point (473nm), u coord
+		abv = 0.073581; // color axis begining point (473nm), v coord
+		aeu = 0.471899; // color axis ending point (574nm), u coord
+		aev = 0.527051; // color axis ending point (574nm), v coord
+	blind.deutan =
+		cpu =  1.14; // confusion point, u coord
+		cpv = -0.14; // confusion point, v coord
+		abu = 0.102776; // color axis begining point (477nm), u coord
+		abv = 0.102864; // color axis begining point (477nm), v coord
+		aeu = 0.505845; // color axis ending point (579nm), u coord
+		aev = 0.493211; // color axis ending point (579nm), v coord
+	blind.tritan =
+		cpu =  0.171; // confusion point, u coord
+		cpv = -0.003; // confusion point, v coord
+		abu = 0.045391; // color axis begining point (490nm), u coord
+		abv = 0.294976; // color axis begining point (490nm), v coord
+		aeu = 0.665764; // color axis ending point (610nm), u coord
+		aev = 0.334011; // color axis ending point (610nm), v coord
+			
+	m = (aev - abv) / (aeu - abu); // slope of color axis
+	yi = blind[t].abv - blind[t].abu * blind[t].m; // "y-intercept" of axis (on the "v" axis at u=0)
+
+*/
+
+// Blind.js
+import React, { useRef, ChangeEvent, useState } from "react";
+import "./index.css";
+
+const colorBlindSimulations = [
+  {
+    id: "cb-canvasProtanomaly",
+    label: "Protanomaly",
+    type: "protan",
+    anomalize: true,
+    blinder: { x: 0.7465, y: 0.2535, m: 1.273463, yi: -0.073894 },
+  },
+  {
+    id: "cb-canvasProtanopia",
+    label: "Protanopia",
+    type: "protan",
+    anomalize: false,
+    blinder: { x: 0.7465, y: 0.2535, m: 1.273463, yi: -0.073894 },
+  },
+  {
+    id: "cb-canvasDeuteranomaly",
+    label: "Deuteranomaly",
+    type: "deutan",
+    anomalize: true,
+    blinder: { x: 1.4, y: -0.4, m: 0.968437, yi: 0.003331 },
+  },
+  {
+    id: "cb-canvasDeuteranopia",
+    label: "Deuteranopia",
+    type: "deutan",
+    anomalize: false,
+    blinder: { x: 1.4, y: -0.4, m: 0.968437, yi: 0.003331 },
+  },
+  {
+    id: "cb-canvasTritanomaly",
+    label: "Tritanomaly",
+    type: "tritan",
+    anomalize: true,
+    blinder: { x: 0.1748, y: 0.0, m: 0.062921, yi: 0.292119 },
+  },
+  {
+    id: "cb-canvasTritanopia",
+    label: "Tritanopia",
+    type: "tritan",
+    anomalize: false,
+    blinder: { x: 0.1748, y: 0.0, m: 0.062921, yi: 0.292119 },
+  },
+  {
+    id: "cb-canvasAchromatomaly",
+    label: "Achromatomaly",
+    type: "achroma",
+    anomalize: true,
+    blinder: {},
+  },
+  {
+    id: "cb-canvasAchromatopsia",
+    label: "Achromatopsia",
+    type: "achroma",
+    anomalize: false,
+    blinder: {},
+  },
+];
+
+// Color blindness simulation vertex shader
+const vertexShaderSourceCB = `
+  attribute vec2 a_position;
+  attribute vec2 a_texCoord;
+  varying vec2 v_texCoord;
+  void main(){
+    gl_Position = vec4(a_position, 0.0, 1.0);
+    v_texCoord = a_texCoord;
+  }
+`;
+
+// Color blindness simulation fragment shader
+const fragmentShaderSourceCB = `
+  precision mediump float;
+  uniform sampler2D u_image;
+  // u_blinder: (x, y, m, yi)
+  uniform vec4 u_blinder;
+  uniform bool u_anomalize;
+  uniform bool u_achroma;
+  uniform float u_gamma;
+  varying vec2 v_texCoord;
+
+  const mat3 matrixRgbXyz = mat3(
+    0.41242371206635076, 0.3575793401363035, 0.1804662232369621,
+    0.21265606784927693, 0.715157818248362, 0.0721864539171564,
+    0.019331987577444885, 0.11919267420354762, 0.9504491124870351
+  );
+  const mat3 matrixXyzRgb = mat3(
+    3.240712470389558, -1.5372626602963142, -0.49857440415943116,
+    -0.969259258688888, 1.875996969313966, 0.041556132211625726,
+    0.05563600315398933, -0.2039948802843549, 1.0570636917433989
+  );
+
+  vec3 toLinear(vec3 c) {
+    vec3 linear;
+    linear.r = (c.r <= 0.04045) ? c.r / 12.92 : pow((c.r + 0.055)/1.055, 2.4);
+    linear.g = (c.g <= 0.04045) ? c.g / 12.92 : pow((c.g + 0.055)/1.055, 2.4);
+    linear.b = (c.b <= 0.04045) ? c.b / 12.92 : pow((c.b + 0.055)/1.055, 2.4);
+    return linear;
+  }
+  vec3 toSrgb(vec3 c) {
+    vec3 srgb;
+    srgb.r = (c.r <= 0.0031308) ? 12.92 * c.r : 1.055 * pow(c.r, 1.0/2.4) - 0.055;
+    srgb.g = (c.g <= 0.0031308) ? 12.92 * c.g : 1.055 * pow(c.g, 1.0/2.4) - 0.055;
+    srgb.b = (c.b <= 0.0031308) ? 12.92 * c.b : 1.055 * pow(c.b, 1.0/2.4) - 0.055;
+    return srgb;
+  }
+  vec3 rgbToXyz(vec3 c) {
+    return matrixRgbXyz * c;
+  }
+  vec3 xyzToXyy(vec3 xyz) {
+    float sum = xyz.x + xyz.y + xyz.z;
+    if(sum == 0.0) return vec3(0.0, 0.0, xyz.y);
+    return vec3(xyz.x/sum, xyz.y/sum, xyz.y);
+  }
+  void main(){
+    vec4 texColor = texture2D(u_image, v_texCoord);
+    vec3 c = toLinear(texColor.rgb);
+    if(u_achroma) {
+      float gray = dot(c, vec3(0.212656, 0.715158, 0.072186));
+      vec3 result = vec3(gray);
+      if(u_anomalize) {
+        result = (1.75 * result + c) / 2.75;
+      }
+      gl_FragColor = vec4(toSrgb(result), texColor.a);
+      return;
+    }
+    vec3 xyz = rgbToXyz(c);
+    vec3 xyy = xyzToXyy(xyz);
+    float slope = (xyy.y - u_blinder.y) / (xyy.x - u_blinder.x);
+    float yi = xyy.y - xyy.x * slope;
+    float dx = (u_blinder.w - yi) / (slope - u_blinder.z);
+    float dy = slope * dx + yi;
+    float simX = dx * xyy.z / dy;
+    float simY = xyy.z;
+    float simZ = (1.0 - (dx + dy)) * xyy.z / dy;
+    float ngx = 0.312713 * xyy.z / 0.329016;
+    float ngz = 0.358271 * xyy.z / 0.329016;
+    float dX = ngx - simX;
+    float dZ = ngz - simZ;
+    vec3 dRGB;
+    dRGB.r = dX * matrixXyzRgb[0][0] + dZ * matrixXyzRgb[0][2];
+    dRGB.g = dX * matrixXyzRgb[1][0] + dZ * matrixXyzRgb[1][2];
+    dRGB.b = dX * matrixXyzRgb[2][0] + dZ * matrixXyzRgb[2][2];
+    vec3 simRGB;
+    simRGB.r = simX * matrixXyzRgb[0][0] + simY * matrixXyzRgb[0][1] + simZ * matrixXyzRgb[0][2];
+    simRGB.g = simX * matrixXyzRgb[1][0] + simY * matrixXyzRgb[1][1] + simZ * matrixXyzRgb[1][2];
+    simRGB.b = simX * matrixXyzRgb[2][0] + simY * matrixXyzRgb[2][1] + simZ * matrixXyzRgb[2][2];
+    float _r = ((simRGB.r < 0.0 ? 0.0 : 1.0) - simRGB.r) / dRGB.r;
+    float _g = ((simRGB.g < 0.0 ? 0.0 : 1.0) - simRGB.g) / dRGB.g;
+    float _b = ((simRGB.b < 0.0 ? 0.0 : 1.0) - simRGB.b) / dRGB.b;
+    _r = (_r > 1.0 || _r < 0.0) ? 0.0 : _r;
+    _g = (_g > 1.0 || _g < 0.0) ? 0.0 : _g;
+    _b = (_b > 1.0 || _b < 0.0) ? 0.0 : _b;
+    float adjust = max(_r, max(_g, _b));
+    simRGB += adjust * dRGB;
+    simRGB = clamp(simRGB, 0.0, 1.0);
+    simRGB = pow(simRGB, vec3(1.0/u_gamma));
+    if(u_anomalize) {
+      simRGB = (1.75 * simRGB + c) / 2.75;
+    }
+    gl_FragColor = vec4(toSrgb(simRGB), texColor.a);
+  }
+`;
+
+// Init WebGL
+function initColorBlindGL(
+  canvas: HTMLCanvasElement,
+  simSetting:
+    | {
+        id: string;
+        label: string;
+        type: string;
+        anomalize: boolean;
+        blinder: { x: number; y: number; m: number; yi: number };
+      }
+    | {
+        id: string;
+        label: string;
+        type: string;
+        anomalize: boolean;
+        blinder: {
+          x?: undefined;
+          y?: undefined;
+          m?: undefined;
+          yi?: undefined;
+        };
+      },
+  image: HTMLImageElement
+) {
+  const gl = canvas.getContext("webgl");
+  if (!gl) {
+    console.error("WebGL Not Support");
+    return;
+  }
+  canvas.width = image.width;
+  canvas.height = image.height;
+  gl.viewport(0, 0, canvas.width, canvas.height);
+
+  const vShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSourceCB);
+  const fShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSourceCB);
+  const program = createProgram(gl, vShader!, fShader!);
+  gl.useProgram(program);
+
+  // Upload full screen quad data
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, quadVertices, gl.STATIC_DRAW);
+  const a_position = gl.getAttribLocation(program!, "a_position");
+  const a_texCoord = gl.getAttribLocation(program!, "a_texCoord");
+  gl.enableVertexAttribArray(a_position);
+  gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 16, 0);
+  gl.enableVertexAttribArray(a_texCoord);
+  gl.vertexAttribPointer(a_texCoord, 2, gl.FLOAT, false, 16, 8);
+
+  // Create texture upload image
+  const texture = gl.createTexture();
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+  // Set uniform vars
+  const u_image = gl.getUniformLocation(program!, "u_image");
+  gl.uniform1i(u_image, 0);
+  const u_blinder = gl.getUniformLocation(program!, "u_blinder");
+  if (simSetting.blinder) {
+    gl.uniform4f(
+      u_blinder,
+      simSetting.blinder.x || 0.0,
+      simSetting.blinder.y || 0.0,
+      simSetting.blinder.m || 0.0,
+      simSetting.blinder.yi || 0.0
+    );
+  } else {
+    gl.uniform4f(u_blinder, 0.0, 0.0, 0.0, 0.0);
+  }
+  const u_anomalize = gl.getUniformLocation(program!, "u_anomalize");
+  gl.uniform1i(u_anomalize, simSetting.anomalize ? 1 : 0);
+  const u_achroma = gl.getUniformLocation(program!, "u_achroma");
+  gl.uniform1i(u_achroma, simSetting.type === "achroma" ? 1 : 0);
+  const u_gamma = gl.getUniformLocation(program!, "u_gamma");
+  gl.uniform1f(u_gamma, 2.2);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+const visualEffects = [
+  { id: "vi-canvasOriginal", effect: 0, label: "Original" },
+  { id: "vi-canvasCataracts", effect: 1, label: "Cataracts" },
+  { id: "vi-canvasGlaucoma", effect: 2, label: "Glaucoma" },
+  { id: "vi-canvasLowvision", effect: 3, label: "Lowvision" },
+  { id: "vi-canvasNightshift", effect: 4, label: "Nightshift" },
+  { id: "vi-canvasSunlight", effect: 5, label: "Sunlight" },
+];
+
+// Vision Impairment simulation vertex shader
+const vertexShaderSourceVI = `
+  attribute vec2 a_position;
+  attribute vec2 a_texCoord;
+  varying vec2 v_texCoord;
+  void main(){
+    gl_Position = vec4(a_position, 0.0, 1.0);
+    v_texCoord = a_texCoord;
+  }
+`;
+
+// Vision Impairment, based on u_effect
+const fragmentShaderSourceVI = `
+  precision mediump float;
+  varying vec2 v_texCoord;
+  uniform sampler2D u_image;
+  uniform int u_effect;
+  uniform vec2 u_texelSize;
+
+  vec3 adjustSaturation(vec3 color, float saturation) {
+      float gray = dot(color, vec3(0.299, 0.587, 0.114));
+      return mix(vec3(gray), color, saturation);
+  }
+
+  vec3 blur9() {
+      vec3 sum = vec3(0.0);
+      for (int x = -1; x <= 1; x++) {
+          for (int y = -1; y <= 1; y++) {
+              vec2 offset = vec2(float(x), float(y)) * u_texelSize;
+              sum += texture2D(u_image, v_texCoord + offset).rgb;
+          }
+      }
+      return sum / 12.0;
+  }
+
+  void main(){
+    vec3 color = texture2D(u_image, v_texCoord).rgb;
+    if(u_effect == 0) {
+      gl_FragColor = vec4(color, 1.0);
+    } else if(u_effect == 1) {
+      vec3 blurred = blur9();
+      float noise = fract(sin(dot(v_texCoord.xy, vec2(20.9898,78.233))) * 43758.5453);
+      vec3 haze = mix(blurred, vec3(1.0), 0.4 * noise);
+      gl_FragColor = vec4(haze, 1.0);
+    } else if(u_effect == 2) {
+      vec3 blurred = blur9();
+      float dist = distance(v_texCoord, vec2(0.5, 0.5));
+      float mask = 1.0 - smoothstep(0.0, 0.4, dist);
+      vec3 result = mix(blurred, vec3(1.0), mask * 0.5);
+      gl_FragColor = vec4(result, 1.0);
+    } else if(u_effect == 3) {
+      vec3 sum = vec3(0.0);
+      for (int x = -2; x <= 2; x++) {
+          for (int y = -2; y <= 2; y++) {
+              vec2 offset = vec2(float(x), float(y)) * u_texelSize;
+              sum += texture2D(u_image, v_texCoord + offset).rgb;
+          }
+      }
+      vec3 strongerBlur = sum / 25.0;
+      gl_FragColor = vec4(strongerBlur, 1.0);
+    } else if(u_effect == 4) {
+      vec3 sat = adjustSaturation(color, 1.2);
+      vec3 warm = sat * vec3(1.05, 1.0, 0.95) + vec3(0.05, 0.0, 0.0);
+      gl_FragColor = vec4(warm, 1.0);
+    } else if(u_effect == 5) {
+      vec3 bright = color * 1.2;
+      float grad = clamp((v_texCoord.x + v_texCoord.y) * 0.5, 0.0, 1.0);
+      vec3 result = mix(bright, bright + vec3(0.1), grad);
+      gl_FragColor = vec4(result, 1.0);
+    } else {
+      gl_FragColor = vec4(color, 1.0);
+    }
+  }
+`;
+
+// Init WebGL for Vision Impairment
+function initVIWebGL(
+  canvas: HTMLCanvasElement,
+  effectNum: number,
+  image: HTMLImageElement
+) {
+  const gl = canvas.getContext("webgl");
+  if (!gl) {
+    console.error("WebGL Unsupported");
+    alert("WebGL Unsupported");
+    return;
+  }
+  canvas.width = image.width;
+  canvas.height = image.height;
+  gl.viewport(0, 0, canvas.width, canvas.height);
+
+  const vShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSourceVI);
+  const fShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSourceVI);
+  const program = createProgram(gl, vShader!, fShader!);
+  gl.useProgram(program);
+
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, quadVertices, gl.STATIC_DRAW);
+  const a_position = gl.getAttribLocation(program!, "a_position");
+  const a_texCoord = gl.getAttribLocation(program!, "a_texCoord");
+  gl.enableVertexAttribArray(a_position);
+  gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 16, 0);
+  gl.enableVertexAttribArray(a_texCoord);
+  gl.vertexAttribPointer(a_texCoord, 2, gl.FLOAT, false, 16, 8);
+
+  const texture = gl.createTexture();
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+  const u_image = gl.getUniformLocation(program!, "u_image");
+  gl.uniform1i(u_image, 0);
+  const u_effect = gl.getUniformLocation(program!, "u_effect");
+  gl.uniform1i(u_effect, effectNum);
+  const u_texelSize = gl.getUniformLocation(program!, "u_texelSize");
+  gl.uniform2f(u_texelSize, 1.0 / canvas.width, 1.0 / canvas.height);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+const quadVertices = new Float32Array([
+  -1, -1, 0, 0, 1, -1, 1, 0, -1, 1, 0, 1, -1, 1, 0, 1, 1, -1, 1, 0, 1, 1, 1, 1,
+]);
+
+function createShader(gl: WebGLRenderingContext, type: number, source: string) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader!, source);
+  gl.compileShader(shader!);
+  if (!gl.getShaderParameter(shader!, gl.COMPILE_STATUS)) {
+    console.error("Shader compile error:", gl.getShaderInfoLog(shader!));
+    gl.deleteShader(shader);
+    return null;
+  }
+  return shader;
+}
+
+function createProgram(
+  gl: WebGLRenderingContext,
+  vertexShader: WebGLShader,
+  fragmentShader: WebGLShader
+) {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error("Program link error:", gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+    return null;
+  }
+  return program;
+}
+
+
+function Simulations() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [loading, setLoading] = useState(false);
+  const [rendered, setRendered] = useState(false);
+
+  const processFile = (file: Blob) => {
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      const img = new Image();
+      img.onload = () => {
+        // colorBlindSimulations
+        colorBlindSimulations.forEach((sim) => {
+          const canvas = document.getElementById(sim.id) as HTMLCanvasElement;
+          if (canvas) initColorBlindGL(canvas, sim, img);
+        });
+        // visualEffects
+        visualEffects.forEach((effect) => {
+          const canvas = document.getElementById(
+            effect.id
+          ) as HTMLCanvasElement;
+          if (canvas) initVIWebGL(canvas, effect.effect, img);
+        });
+      };
+      if (!!evt?.target?.result) {
+        img.src = evt.target.result as string;
+      } else {
+        img.src = "";
+      }
+    };
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setLoading(false);
+      setRendered(true);
+    };
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setLoading(true);
+      setRendered(false);
+      processFile(e.target.files[0]);
+    }
+  };
+
+  const handleCardClick = () => {
+    if (fileInputRef.current) fileInputRef.current.click();
+  };
+
+  return (
+    <div className={`simulation`}>
+      <div
+        className={`upload-card ${loading ? "upload-card__loading" : ""}`}
+        onClick={handleCardClick}>
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          style={{ display: "none" }}
+          disabled={loading}
+          onChange={handleFileChange}
+        />
+        <p>{loading ? "Loading..." : "Upload a photo or design screenshot here"}</p>
+      </div>
+      {/* Vision Impairment */}
+      <div className={`simulation-section ${rendered? 'photo__rendered': ''}`}>
+        <h3>Vision Impairment</h3>
+        <div id="visualImpairmentContainer" className={`container`} tabIndex={rendered? -1: 1}>
+          {visualEffects.map((effect) => (
+            <div className="canvasItem" key={effect.id}>
+              <canvas id={effect.id}></canvas>
+              <p className="type-label" aria-label={effect.label}>{effect.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Color Blind */}
+      <div className={`simulation-section ${rendered? 'photo__rendered': ''}`} tabIndex={rendered? -1: 1}>
+        <h3>Color Blind</h3>
+        <div id="colorBlindContainer" className={`container`}>
+          {colorBlindSimulations.map((sim) => (
+            <div className="simResult" key={sim.id}>
+              <canvas id={sim.id}></canvas>
+              <p>{sim.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Simulations;
---
+++ src/components/main/index.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React, { useState } from "react";
+import Blind from "../blind";
+import "./index.css";
+import "./gradient-text.css";
+import "./dark.css";
+
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <div className={`app-container ${isDarkMode ? "dark" : ""}`}>
+      {/* header */}
+      <header className="header" aria-label="header">
+        <div className="logo" aria-label="logo">
+          What Can See
+        </div>
+        <button
+          className="dark-mode-toggle"
+          onClick={toggleDarkMode}
+          aria-label={`Switch to ${isDarkMode ? "Day Mode" : "Dark Mode"}`}
+          aria-pressed={isDarkMode}>
+          {isDarkMode ? "Day Mode" : "Dark Mode"}
+        </button>
+      </header>
+
+      {/* Main */}
+      <main className="main">
+        {/* hero */}
+        <div className="hero title">
+          <h1>What Can See</h1>
+          <p>View Without Barriers, Embrace Every Perspective.</p>
+        </div>
+
+        {/* Cards */}
+        <div className="cards">
+          <Blind />
+        </div>
+      </main>
+      <footer>
+        <h3>Website Introduction</h3>
+        <ul>
+          <li>
+            Welcome to <b><i>What Can See</i></b> – a web tool that you can upload any image to see how it appears to people with visual impairments and color blindness.
+          </li>
+          <li>
+            Our mission is to emphasize the importance of accessible design by
+            empowering photographers, web, and app designers.
+          </li>
+          <li>
+            Our simulations are inspired by open source projects and
+            other websites, so we do not claim to represent the exact
+            experiences of visually impaired and colorblind
+            individuals. Just hope it can illuminate something to
+            guide inclusive design practices.
+          </li>
+          <li>
+            Inspiration from projects like <a href="https://www.whocanuse.com/?utm_source=What+Can+see" referrerPolicy="no-referrer">Who Can Use</a> and <a href="http://colorlab.wickline.org/colorblind/colorlab?utm_source=What+Can+see" referrerPolicy="no-referrer">ColorLab by
+            Wickline</a>, <b><i>What Can See</i></b> is here to help you design digital
+            experiences that are accessible to everyone.
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
